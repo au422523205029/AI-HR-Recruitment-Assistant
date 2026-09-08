532fed18ff65a6d8b445b8544bf4fd94a2c65978
Ishwarya.A <ishwaryaanandh71@gmail.com>
2026-09-08 08:34:43 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,7 +1,7 @@
 import os
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
-from .schemas import CandidateScore, InterviewQuestions
+from schemas import CandidateScore,InterviewQuestions
 
 def llm():
     return ChatOpenAI(model=os.getenv("OPENAI_MODEL","gpt-4o-mini"),temperature=0.1)
